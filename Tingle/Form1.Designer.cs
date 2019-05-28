b8689163c28562ebfba72c16772402891db09bd0
Cornell Reddick <cornellreddick@outlook.com>
2019-05-28 13:37:31 -0400

    Simple WFP to display Wingdings

--- Tingle/Form1.Designer.cs
+++ Tingle/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tingle
 {
-    partial class Form1
+    partial class Wingdings
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // firstButton
+            // 
+            this.firstButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.firstButton.Location = new System.Drawing.Point(26, 113);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(75, 23);
+            this.firstButton.TabIndex = 0;
+            this.firstButton.Text = "Display";
+            this.firstButton.UseCompatibleTextRendering = true;
+            this.firstButton.UseVisualStyleBackColor = false;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.resultsLabel.Font = new System.Drawing.Font("Wingdings", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.resultsLabel.Location = new System.Drawing.Point(21, 59);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(0, 30);
+            this.resultsLabel.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Wingdings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.firstButton);
+            this.Name = "Wingdings";
+            this.Text = "Wingdings";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Tingle/Form1.cs
+++ Tingle/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace Tingle
 {
-    public partial class Form1 : Form
+    public partial class Wingdings : Form
     {
-        public Form1()
+        public Wingdings()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var results = textBox1.Text = "";
+            resultsLabel.Text = results;
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            var results = textBox1.Text;
+            resultsLabel.Text = results;
+            textBox1.Clear();
         }
     }
 }
